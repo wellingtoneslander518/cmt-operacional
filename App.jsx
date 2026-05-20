e9f174e5a35f0e230e2eda848fc65c7586046a9c
wellingtoneslander518 <wellingtoneslander518@gmail.com>
2026-05-20 14:27:29 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,11 @@
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>CMT Operacional</title>
-  <script type="module" src="/src/main.jsx"></script>
-</head>
-<body>
-  <div id="root"></div>
-</body>
-</html>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
