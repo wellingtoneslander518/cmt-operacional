3dc5742165a7f6eca0cade3f20cbbfb506678039
wellingtoneslander518 <wellingtoneslander518@gmail.com>
2026-05-20 14:15:26 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>CMT Operacional</title>
+  <script type="module" src="/src/main.jsx"></script>
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>
